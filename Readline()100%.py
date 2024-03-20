dcec4a172612884bb7f0249923e9bbd7bcb5e18c
f-caro <fa.caro.aguilera@gmail.com>
2024-03-20 00:48:43 -0300

    testing out parseRows and parseRowTrack

--- Readline()100%.py
+++ Readline()100%.py
@@ -7,6 +7,30 @@ pyb.freq(100000000)
 uart2 = pyb.UART(2, 38400)
 uart2.init(baudrate=38400, bits=8, parity=None, stop=1, timeout=261, flow=0, timeout_char=261, read_buf_len=339)
 
+#from a single row, cut out the important info, return num & dict
+def parseRowTrack(row_track):
+    trackDict = {}
+    track_num = row_track[0:3]
+    track_valido = row_track[13:16]
+    ieee754_value = row_track[-4:]  # might not work, so maybe  row_track[17:20]
+    trackDict = { "track_valido": track_valido , "ieee754_value" : ieee754_value }
+    return track_num , trackDict
+
+#parse all rows, return Dictionary of each track as key
+def parseRows( last_section):
+    #0 .. 15
+    all_tracks = {}
+    for i in range(16):
+        track_num = ""
+        trackDict = {}
+        track_num , trackDict = parseRowTrack( last_section[i*20 : (i+1)*20] )
+        all_tracks[track_num] = trackDict
+        
+    if(last_section[16*20 + 1:] == b'\xA5\xA5' ):
+        print("message_ends")
+    
+    return all_tracks
+
 # Función para transmitir un mensaje por UART2
 def transmit_message(message):
     uart2.write(message)
@@ -30,6 +54,9 @@ while True:
         # Verificar el valor del primer arreglo
         if section1 == b'\x5A\x5A\x01\x4C':
             # Transmitir solo la sección 2
+            all_tracks = {}
+            all_tracks = parseRows(section3)
+            print(all_tracks)
             transmit_message(section2)
         else:
             # Transmitir toda la palabra recibida
