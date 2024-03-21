89c706061724786b562a78b611e39c5bcd6ba969
f-caro <fa.caro.aguilera@gmail.com>
2024-03-21 01:25:53 -0300

    needed to change TxMsg string output to work with Array and not dict

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ pyb.freq(100000000)
 # Configurar UART2 para recepción y transmisión a 38400 baudios
 uart2 = pyb.UART(2, 38400)
 uart2.init(baudrate=38400, bits=8, parity=None, stop=1, timeout=261, flow=0, timeout_char=261)
+
 #from a single row, cut out the important info, return num & dict
 def parseRowTrack(row_track):
     trackDict = {}
@@ -14,6 +15,7 @@ def parseRowTrack(row_track):
     trackDict["ieee754_value"] = ieee754_value
     print(track_num , " :: ", trackDict )
     return track_num , trackDict
+
 #parse all rows, return Dictionary of each track as key
 def parseRows(last_section):
     #0 .. 15
@@ -59,9 +61,15 @@ def prepareTxMsg(all_tracks):
         bcd_value = None
     msgStr = ",".join(msgArr)
     return msgStr
+
+def prepareArrForTxMsg(all_tracks_arr):
+    msgStr = ",".join(all_tracks_arr)
+    return msgStr
+
 # Función para transmitir un mensaje por UART2
 def transmit_message(message):
     uart2.write(message)
+
 # Bucle principal
 while True:
     # Leer mensaje de UART2
@@ -84,8 +92,7 @@ while True:
         # Procesar las secciones del primer mensaje
         # Verificar el valor del primer arreglo
         if section1 == b'\x5A\x5A\x01\x4C':
-            # Transmitir solo la sección 2
-            
+            # Transmitir solo la sección 3
             all_tracks = all_tracks + prepareTxArr(parseRows(section3))
         else:
             # Transmitir toda la palabra recibida
@@ -105,8 +112,7 @@ while True:
             
             # Procesar las secciones del mensaje restante
             if section1 == b'\x5A\x5A\x01\x4C':
-                # Transmitir solo la sección 2
-                
+                # Transmitir solo la sección 3
                 all_tracks = all_tracks + prepareTxArr(parseRows(section3))
             else:
                 # Transmitir toda la palabra recibida
@@ -118,6 +124,6 @@ while True:
         # Transmitir los datos procesados (si es necesario)
         if all_tracks:
             # Realizar la transmisión de los datos procesados
-            msgTx = "$" + prepareTxMsg(all_tracks) + "*"
+            msgTx = "$" + prepareArrForTxMsg(all_tracks) + "*"
             print("mensaje para TX ::::  ",  msgTx )
             transmit_message(msgTx)
